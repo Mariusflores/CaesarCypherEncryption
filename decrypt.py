7789a041df23cf145e74f8f429dd042613081df3
Mariusflores <mada060@student.kristiania.no>
2023-05-11 19:44:01 +0200

    Finished encryption and decryption

--- decrypt.py
+++ decrypt.py
@@ -1,6 +1,6 @@
 
-file_to_encrypt = open("sample.txt", "r")
-encrypted_file = open("encrypted.txt", "w")
+file_to_encrypt = open("encrypted.txt", "r")
+decrypted_file = open("decrypted.txt", "w")
 file_text = file_to_encrypt.read()
 
 
@@ -12,11 +12,11 @@ def caesar_cypher(plain_text, shift):
     for char in plain_text:
         if char.isalpha():
             char_code = ord(char)
-            shifted_code = (char_code + shift - 65) % 26 + 65
+            shifted_code = (char_code - shift - 65) % 26 + 65
             cipher_text += chr(shifted_code)
         else:
             cipher_text += char
-    encrypted_file.write(cipher_text)
+    decrypted_file.write(cipher_text)
 
 
 caesar_cypher(file_text, 4)
